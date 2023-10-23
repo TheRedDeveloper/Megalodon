feb09bb6a73b84400d0d098f7e304169ad657423
RedDev <50879531+TheRedDeveloper@users.noreply.github.com>
2023-10-23 10:31:32 +0200

    Moving Square!

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float RotationSpeed = 2;
-
-    void Start()
-    {
-        
-    }
+    public float RotationHardness = 22.5f;
+    public float TranslationSpeed = 2;
+    public Transform PlayerSprite;
 
     void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime * -RotationSpeed;
+        if(!PlayerSprite) Debug.LogError("Movement kann PlayerSprite nicht finden.");
+        PlayerSprite.localEulerAngles = new Vector3(0, 0, 30 * -Input.GetAxisRaw("Horizontal"));
         transform.Rotate(0, 0, speed);
+        transform.Translate(Vector3.up * Time.deltaTime * TranslationSpeed * Input.GetAxisRaw("Vertical"), Camera.main.transform);
     }
 }
