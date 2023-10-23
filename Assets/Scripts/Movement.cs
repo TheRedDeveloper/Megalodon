db32836f1cb62a038aba2c2e9a75e72424417b95
RedDev <50879531+TheRedDeveloper@users.noreply.github.com>
2023-10-23 11:25:19 +0200

    Added BattleScene

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,15 +7,16 @@ public class Movement : MonoBehaviour
     public float RotationSpeed = 2;
     public float RotationHardness = 22.5f;
     public float TranslationSpeed = 2;
+    public float slowdownFactor = .5f;
     public Transform PlayerSprite;
 
     void Update()
     {
         if(!PlayerSprite) Debug.LogError("Movement kann PlayerSprite nicht finden.");
-        PlayerSprite.localEulerAngles = new Vector3(0, 0, 30 * -Input.GetAxisRaw("Horizontal"));
+        PlayerSprite.localEulerAngles = new Vector3(0, 0, RotationHardness * -Input.GetAxisRaw("Horizontal"));
         transform.Rotate(0, 0, Input.GetAxisRaw("Horizontal") * Time.deltaTime * -RotationSpeed);
         float speedCoeff = 1;
-        if(Input.GetAxisRaw("Horizontal") != 0f | Input.GetAxisRaw("Vertical") < 0f) speedCoeff = 0.5f;
+        if(Input.GetAxisRaw("Horizontal") != 0f | Input.GetAxisRaw("Vertical") < 0f) speedCoeff = slowdownFactor;
         transform.Translate(Vector3.up * Time.deltaTime * TranslationSpeed * Input.GetAxisRaw("Vertical") * speedCoeff, Camera.main.transform);
     }
 }
