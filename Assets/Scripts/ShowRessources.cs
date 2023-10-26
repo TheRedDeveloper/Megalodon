d7a198cda7b0308924761ff6c8b495a0b1c83502
RedDev <50879531+TheRedDeveloper@users.noreply.github.com>
2023-10-26 10:32:32 +0200

    Stats UI Rework

--- Assets/Scripts/ShowRessources.cs
+++ Assets/Scripts/ShowRessources.cs
@@ -8,11 +8,8 @@ public class ShowRessources : MonoBehaviour
 {
     public TMP_Text level;
     public TMP_Text metal;
-    public TMP_Text metalR;
     public TMP_Text oil;
-    public TMP_Text oilR;
     public TMP_Text gunpowder;
-    public TMP_Text gunpowderR;
 
     public static int[] requiredMetal = {20, 40, 50};
     public static int[] requiredOil = {2, 5, 10};
@@ -21,11 +18,8 @@ public class ShowRessources : MonoBehaviour
     void Update()
     {
         level.text = (Game.level+1).ToString();
-        metal.text = Resources.metal.ToString();
-        metalR.text = requiredMetal[Game.level].ToString();
-        oil.text = Resources.oil.ToString();
-        oilR.text = requiredOil[Game.level].ToString();
-        gunpowder.text = Resources.gunpowder.ToString();
-        gunpowderR.text = requiredGunpowder[Game.level].ToString();
+        metal.text = Resources.metal.ToString() + " / " + requiredMetal[Game.level].ToString();
+        oil.text = Resources.oil.ToString() + " / " + requiredOil[Game.level].ToString();
+        gunpowder.text = Resources.gunpowder.ToString() + " / " + requiredGunpowder[Game.level].ToString();
     }
 }
