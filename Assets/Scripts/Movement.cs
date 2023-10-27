8c7c35484ea800e54b75eb712556ad9a22dce9d5
Max Krumm <39380018+maxkrumm@users.noreply.github.com>
2023-10-27 13:59:57 +0200

    Movement Sound

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class Movement : MonoBehaviour {
     public float RotationSpeed = 2;
@@ -14,11 +15,20 @@ public class Movement : MonoBehaviour {
     public Transform PlayerSprite;
     public Rigidbody2D rb; 
     public bool isMoving = false;
+    private bool soundIsPlaying = false;
+    private FMOD.Studio.EventInstance instance;
 
     void Start(){
         refreshSprite();
+        instance = FMODUnity.RuntimeManager.CreateInstance("event:/UB_Loop");
+        instance.start();
     }
-    void Update(){
+    void OnDestroy()
+    {
+        instance.release();
+    }
+
+    void FixedUpdate(){
         isMoving = Input.GetAxisRaw("Vertical") != 0f || Input.GetAxisRaw("Horizontal") != 0f;
         if(!PlayerSprite) Debug.LogError("Movement kann PlayerSprite nicht finden.");
         float speedCoeff = 1;
@@ -29,7 +39,19 @@ public class Movement : MonoBehaviour {
         if(Input.GetAxisRaw("Horizontal") != 0f | Input.GetAxisRaw("Vertical") < 0f) speedCoeff = slowdownFactor;
         rb.velocity += (Vector2)(Quaternion.AngleAxis(transform.eulerAngles.z, Vector3.forward) * Vector3.up * Time.fixedDeltaTime
         * TranslationSpeed * Input.GetAxisRaw("Vertical") * speedCoeff);
+        if (isMoving && !soundIsPlaying)
+            {
+                instance.setParameterByName("Speed", 1);
+                soundIsPlaying = true;
+            }
+        if (!isMoving && soundIsPlaying)
+            {
+                instance.setParameterByName("Speed", 0);
+                soundIsPlaying = false;
+            }  
+
     }
+
     public void refreshSprite() {
         if(Game.level >= 1) {
             spriteRenderer.sprite = spriteL2;
