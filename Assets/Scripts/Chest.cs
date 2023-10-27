6a925289b1389a409cc8e705c8db2c71e4fc90c2
RedDev <50879531+TheRedDeveloper@users.noreply.github.com>
2023-10-27 11:19:38 +0200

    Neuigkeiten

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -7,12 +7,19 @@ public class Chest : MonoBehaviour {
     public int givesMetal;
     public int givesOil;
     public int givesGunpowder;
+    public Collider2D collider;
+    public SpriteRenderer sr;
+    public Sprite openedChest;
     public int chestId;
 
     void Start(){
         if(Game.openedChests == null) Game.openedChests = new List<int>[]{new List<int>(){}, new List<int>(){}};
         Debug.Log(Game.openedChests[Game.shipwreckId]);
-        if(Game.openedChests[Game.shipwreckId].Contains(chestId)) gameObject.SetActive(false);
+        if(Game.openedChests[Game.shipwreckId].Contains(chestId)){
+            collider.enabled = false;
+            sr.sprite = openedChest;
+            //gameObject.SetActive(false);
+        }
     }
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
@@ -21,7 +28,8 @@ public class Chest : MonoBehaviour {
             Resources.gunpowder += givesGunpowder;
             
             Game.openedChests[Game.shipwreckId].Add(chestId);
-            gameObject.SetActive(false);
+            collider.enabled = false;
+            sr.sprite = openedChest;
         }
     }
 }
